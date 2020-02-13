accd52e1910c050e0a339986fbdb472d26ed1a20
Gianufio83 <fiore.gianuario@gmail.com>
2020-02-13 15:35:58 +0100

    funzione GET

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
-alert('Ciao');
+// alert('Ciao');
+$(document).ready(function () {
+  getAllList();
+});
+
+
+
+
+
+// Functions
+// Function -GET
+function getAllList() {
+  $.ajax({
+    url: 'http://157.230.17.132:3016/todos',
+    method: 'GET',
+    success: function (data) {
+    // console.log(data);
+    var list = data;
+    var source = $('#list-template').html();
+    var template = Handlebars.compile(source);
+    for (var i = 0; i < list.length; i++) {
+       var todo = list[i];
+       var context = {
+         text: todo.text
+       }
+    var html = template(context);
+      $('.lists').append(html);
+    }
+  },
+    error: function () {
+      alert('Errore');
+    }
+  });
+}
